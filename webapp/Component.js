8eb060d3c162d06bc8787206a928a53a3404900b
Harsh Joshi <joshiharsh356@gmail.com>
2026-03-04 05:40:11 +0000

    Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("inv.hj.managecust.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
